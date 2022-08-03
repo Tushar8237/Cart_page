34ad4960cd4d3d1005d4d99938b3fb2cf300e279
Tushar8237 <87122609+Tushar8237@users.noreply.github.com>
2022-08-04 00:02:46 +0530

    cart_page

---
+++ src/component/logIn/LogIn.js
@@ -0,0 +1,50 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import { auth } from "../../firebase";
+import "./logIn.css";
+
+const LogIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async () => {
+    if (!email || !password) {
+      alert("Please fill all the Fields");
+      return;
+    }
+    try {
+      const result = await signInWithEmailAndPassword(auth, email, password);
+      console.warn(result);
+      alert(`Login successful ${result.user.email}`);
+    } catch (error) {
+      alert("error");
+    }
+  };
+
+  return (
+    <div className="login_div">
+      <div className="input_div">
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="login_button">
+        <button className="btn1" type="submit" onClick={handleSubmit}>
+          Login
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LogIn;
